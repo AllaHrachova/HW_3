b9dc09322ee59fcf0cd22f9c692d6d08699ff6b9
AllaHrachova <alla_krivosheina@Ukr.net>
2021-05-03 23:03:57 +0300

    HW_3 fixed

---
+++ ConsoleApp1/ConsoleApp1/InputHelper.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_3
+{
+    class InputHelper
+    {
+        int Min;
+        int Max;
+        public int input;
+        public int element;
+        public bool valid;
+      
+        public int Input
+        {
+            get
+            {
+                return input;
+            }
+            set
+            {
+                if (value < Min)
+                {
+                    valid = false;
+                }
+                else if (value > Max)
+                {
+                    valid = false;
+                }
+                else
+                {
+                    input = value;
+                    valid = true;
+                }
+            }
+        }
+        public int Element
+        {
+            get
+            {
+                return element;
+            }
+            set
+            {
+                element = value;
+            }
+        }
+
+        public  bool Valid
+        { 
+            get
+            {
+                return valid;
+            }
+            set
+            {
+                valid = value;
+            }
+        }
+
+        public InputHelper(int  Min)
+        {
+           do
+            {
+                try
+                {
+                    input  = Convert.ToInt32(Console.ReadLine());
+                    if (input < Min)
+                    {
+                        Console.WriteLine("Введите число не меньше " + Min);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Введите натуральное число не меньше " + Min);
+                }
+            }
+            while (input < Min);
+        }
+
+        public InputHelper()
+        {
+            do
+            {
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                    if (input <= 0)
+                    {
+                        Console.WriteLine("Некорректный ввод. Введите натуральное число");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод. Введите натуральное число");
+                }
+            }
+            while (input <= 0);
+        }
+
+        public InputHelper(int Min, int Max, int Input)
+        {
+            try
+            {
+                if (Input < Min)
+                {
+                    valid = false;
+                    Console.WriteLine("Некорректное значение. Введите число в заданном диапазоне");
+                }
+                else if (Input <= Max)
+                {
+                    valid = true;
+                }
+                if (Input > Max)
+                {
+                    valid = false;
+                    Console.WriteLine("Некорректное значение. Введите число в заданном диапазоне");
+                }
+            }
+        catch
+            {
+                Console.WriteLine("Некорректное значение. Введите натуральное число");
+            }
+        }
+    }
+}
+
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,49 +6,55 @@ namespace HW_3
     {
         static void Main(string[] args)
         {
+            const int MinElementsNumber = 10;
+            int ElementsNumber;
+            int MinElement;
+            int MaxElement;
+            int i = 0;
             int Sum = 0;
-            Console.WriteLine("Из скольких элементов будет состоять массив?");
-            string q_st = Console.ReadLine();
-            ValidInput t = new ValidInput(q_st, 10);
-            int q = t.N;
-            if (t.isvalid)
+            Console.WriteLine("Введите число элементов массива?");
+            InputHelper SetElementsNumber  = new InputHelper(MinElementsNumber);
+            ElementsNumber = SetElementsNumber.Input;
+            do
             {
-                Console.WriteLine("Введите {0} значений", q);
-                int[] Array1 = new int[q];
-                int j = 1;
-
-                for (int i = 0; i < q; i++)
+                Console.WriteLine("Введите минимальный элемент массива");
+                InputHelper SetMinElement = new InputHelper();
+                MinElement = SetMinElement.Input;
+                Console.WriteLine("Введите максимальный элемент массива");
+                InputHelper SetMaxElement = new InputHelper();
+                MaxElement = SetMaxElement.Input;
+                if (MaxElement < MinElement)
                 {
-                    string s = Console.ReadLine();
-                    ValidInput k = new ValidInput(s, 20, 40);
-                    int n = k.N;
-                    if (k.isvalid)
-                    {
-                        Array1[j] = n;
-                        if (k.isvalid)
-                            Console.WriteLine("Значение присвоено элементу массива № {0}", j);
-                        if (Array1[j] % 3 == 0)
-                        {
-                            if (Array1[j] % 5 != 0)
-                            {
-                                Sum = Sum + Array1[j];
-                            }
-                        }
-                        j++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Значение должно быть в диапазоне от 20 до 40");
-                    }
-
+                    Console.WriteLine("Максимальный элемент массива должен быть меньше минимального. ");
                 }
-                Console.WriteLine("Сумма элементов массива - {0}", Sum);
             }
-            else
+            while (MaxElement < MinElement);
+            int[] Array1 = new int[SetElementsNumber.input];
+            do
             {
-                Console.WriteLine("Элементов должно быть не менее 10");
+                Console.WriteLine("Введите значение № " + (i + 1) + " в диапазоне от " + MinElement + " до " + MaxElement);
+                try
+                {
+                    int ArrayElement = Convert.ToInt32(Console.ReadLine());
+                    InputHelper SetArrayElements = new InputHelper(MinElement, MaxElement, ArrayElement);
+                    if (SetArrayElements.valid)
+                    {
+                        Array1[i] = ArrayElement;
+                        //Console.WriteLine("Значение № " + (i + 1) + " = " + Array1[i]);
+                        if ((Array1[i] % 3 == 0) & (Array1[i] % 5 != 0))
+                        {
+                            Sum = Sum + Array1[i];
+                        }
+                        i++;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод. Введите натуральное число");
+                }
             }
-
+            while (i < ElementsNumber);
+            Console.WriteLine("Сумма элементов массива - {0}", Sum);
             Console.ReadLine();
         }
     }
